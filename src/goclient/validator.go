f2114ef6f8c1d337b7b2ef45962ca1a15e67d690
Juan Leni <lenijuan@gmail.com>
2018-08-16 15:56:26 +0200

    Cleaning up integration test code

--- src/goclient/validator.go
+++ src/goclient/validator.go
@@ -1,5 +1,4 @@
 /*******************************************************************************
-*   (c) 2016 Ledger
 *   (c) 2018 ZondaX GmbH
 *
 *  Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,25 +13,9 @@
 *  See the License for the specific language governing permissions and
 *  limitations under the License.
 ********************************************************************************/
-#pragma once
 
-#include <stdbool.h>
-#include "apdu_codes.h"
-#include "common.h"
+package main
 
-#define CLA                         0x56
-
-#define OFFSET_CLA                  0
-#define OFFSET_INS                  1  //< Instruction offset
-#define OFFSET_PCK_INDEX            2  //< Package index offset
-#define OFFSET_PCK_COUNT            3  //< Package count offset
-#define OFFSET_DATA                 5  //< Data offset
-
-#define INS_GET_VERSION                 0
-#define INS_PUBLIC_KEY_ED25519          1
-#define INS_SIGN_ED25519                4
-
-void app_init();
-void app_main();
-void sign(volatile uint32_t *tx);
-
+func main() {
+	println("Hello world!")
+}
